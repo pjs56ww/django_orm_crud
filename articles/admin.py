091be035b9ff585687a07ed32b9da6ee373e11a5
박재성 <sysh5656@naver.com>
2019-09-11 09:05:09 +0900

    Update

--- articles/admin.py
+++ articles/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article, StudentModel
 
 @admin.register(Article)
 class ArticlesAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'content', 'created_at', 'updated_at',)
 
+
+@admin.register(StudentModel)
+class StudentModelsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'birthday', 'age',)
+
+   
 # # Register your models here.
 # admin.site.register(Article)
--- articles/models.py
+++ articles/models.py
@@ -17,3 +17,10 @@ class Article(models.Model):
 
     def __str__(self):
         return f'{self.id}번 글 - {self.title} : {self.content}'
+
+class StudentModel(models.Model):
+
+    name = models.CharField(max_length=20)
+    email = models.CharField(max_length=20)
+    birthday = models.DateField()
+    age = models.IntegerField()
